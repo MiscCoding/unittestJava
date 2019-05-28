9f9c0a8f1d17c456436a590509aa8307d13274e6
neogeoss <neogeo-s@hanmail.net>
2019-05-29 00:46:32 +0900

    Knuth Morris Pratt algorithm table and test case

--- src/test/java/academy/learnprogramming/algorithm/knp/KnuthMorrisPrattTest.java
+++ src/test/java/academy/learnprogramming/algorithm/knp/KnuthMorrisPrattTest.java
@@ -2,10 +2,33 @@ package academy.learnprogramming.algorithm.knp;
 
 import academy.learnprogramming.algorithm.KNP.KnuthMorrisPratt;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class KnuthMorrisPrattTest {
 
+    KnuthMorrisPratt kmp = null;
+    @Before
+    public void init() {
+        kmp = new KnuthMorrisPratt();
+    }
+    @Test
+    public void searchTest() {
+        String array = new String("abazacabababac");
+        String pattern = new String("ababac");
+       int index =  kmp.search(array.toCharArray(), pattern.toCharArray());
+       Assert.assertEquals(8, index);
+
+       array = new String("this is a test");
+       pattern = new String("is a");
+       index = kmp.search(array.toCharArray(), pattern.toCharArray());
+       Assert.assertEquals(5, index);
+
+        array = new String("canfindtheindex");
+        pattern = new String("foo");
+        index = kmp.search(array.toCharArray(), pattern.toCharArray());
+        Assert.assertEquals(-1, index);
+    }
     @Test
     public void computeLSPTableTest(){
         KnuthMorrisPratt kmp = new KnuthMorrisPratt();
